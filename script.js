74098051b97b35022503a1b4e6922f12d0ecc586
Saki Ebina <ebiebi0u0@gmail.com>
2024-07-31 16:40:53 -0700

    finished

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 $(function() {
   // your code here
-})
+
+  const accordionHeader = $('.accordion-header')
+  const accordionContent =$('.accordion-content')
+  const loadTodos = $('.todos')
+
+
+  accordionHeader.on('click',function(){
+    const content = $(this).next('.accordion-content')
+    if (content.is(':visible')) {
+      content.slideUp();
+  } else {
+    accordionContent.slideUp(); 
+    content.slideDown(); 
+  }
+  });
+
+  $('.todos button').on('click',function(){
+      $.ajax ({
+        url:`https://dummyjson.com/todos`,
+        type:`GET`,
+        success: function(response){
+          const todos = response.todos;
+          const todoList = $('.todos ul');
+          todos.forEach(function(todo) {
+            todoList.append('<li>' + todo.todo + '</li>');
+        });
+        },
+        error: function(error) {
+          console.error(error);
+        }
+      })
+ })
+});
